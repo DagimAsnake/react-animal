148b9374a78cb5bc01d52f04a6b1680086c2cdcb
Dagim Asnake <dagimasnake1@gmail.com>
2023-11-27 06:48:04 -1200

    finished features

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import fly from "../assets/fly.png";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { RiRobot2Line } from "react-icons/ri";
+import { GoGear } from "react-icons/go";
+import { CiShare2 } from "react-icons/ci";
 
 const Features = () => {
   return (
@@ -18,57 +20,57 @@ const Features = () => {
                         <div className='grid grid-cols-2 m-5 gap-1'>
                             <div className='w-full md:w-80 p-2 md:p-4  border-2 dark:border-gray-800 rounded-xl'>
                                 <div className='flex justify-between items-center'>
-                                    <p className='text-xl dark:text-white'>Breed</p>
+                                    <p className='text-md md:text-xl dark:text-white'>Breed</p>
                                     <CiLocationArrow1 className='dark:text-white' />
                                 </div>
-                                <p className='text-2xl font-semibold dark:text-white'>Akita</p>
+                                <p className='text-xl md:text-2xl font-semibold dark:text-white'>Akita</p>
                             </div>
 
-                            <div className='w-full md:w-80 p-2 md:p-4 border-2 dark:border-gray-800 rounded-xl'>
+                            <div className='w-full lg:w-80 p-2 md:p-4 border-2 dark:border-gray-800 rounded-xl'>
                                 <div className='flex justify-between items-center'>
-                                    <p className='text-xl dark:text-white'>Breed</p>
+                                    <p className='text-md md:text-xl dark:text-white'>Prompt</p>
                                     <CiLocationArrow1 className='dark:text-white' />
                                 </div>
-                                <p className='text-2xl font-semibold dark:text-white'>Akita</p>
+                                <p className='text-xl md:text-2xl font-semibold dark:text-white'>Nothing</p>
                             </div>
                         </div>
 
-                        <div className='grid grid-cols-2 m-5'>
+                        <div className='grid grid-cols-2 gap-1 m-5'>
                             <div className='w-full md:w-80 p-2 md:p-4 border-2 dark:border-gray-800 rounded-xl'>
                                 <div className='flex justify-between items-center'>
-                                    <p className='text-xl dark:text-white'>Breed</p>
+                                    <p className='text-md md:text-xl dark:text-white'>Style</p>
                                     <CiLocationArrow1 className='dark:text-white' />
                                 </div>
-                                <p className='text-2xl font-semibold dark:text-white'>Akita</p>
+                                <p className='text-xl md:text-2xl font-semibold dark:text-white'>Anime</p>
                             </div>
 
-                            <div className='w-full md:w-80 p-2 md:p-4 border-2 dark:border-gray-800 rounded-xl'>
+                            <div className='w-full lg:w-80 p-2 md:p-4 border-2 dark:border-gray-800 rounded-xl'>
                                 <div className='flex justify-between items-center'>
-                                    <p className='text-xl dark:text-white'>Breed</p>
+                                    <p className='text-md md:text-xl dark:text-white'>Colour</p>
                                     <CiLocationArrow1 className='dark:text-white' />
                                 </div>
-                                <p className='text-2xl font-semibold dark:text-white'>Akita</p>
+                                <p className='text-xl md:text-2xl font-semibold dark:text-white'>Brindle</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='md:mt-20'>
                     <div>
-                        <RiRobot2Line className='text-4xl dark:text-white' />
+                        <RiRobot2Line className='text-4xl dark:text-white font-bold' />
                         <h1 className='text-3xl font-bold dark:text-white mb-5'>AI Powered:</h1>
                         <p className='text-lg dark:text-white'>Craft distinct pet icons each time using OpenAI's DALL-E2 technology, ensuring unparalleled designs for your endeavors.</p>
                     </div>
 
                     <div className='my-8'>
-                        <RiRobot2Line className='text-4xl dark:text-white' />
-                        <h1 className='text-3xl font-bold dark:text-white mb-5'>AI Powered:</h1>
-                        <p className='text-lg dark:text-white'>Craft distinct pet icons each time using OpenAI's DALL-E2 technology, ensuring unparalleled designs for your endeavors.</p>
+                        <CiShare2 className='text-4xl dark:text-white font-bold' />
+                        <h1 className='text-3xl font-bold dark:text-white mb-5'>Customization:</h1>
+                        <p className='text-lg dark:text-white'>Effortlessly tailor your pet icons with our user-friendly tools, enabling you to modify hues, designs, and movements.</p>
                     </div>
 
                     <div>
-                        <RiRobot2Line className='text-4xl dark:text-white' />
-                        <h1 className='text-3xl font-bold dark:text-white mb-5'>AI Powered:</h1>
-                        <p className='text-lg dark:text-white'>Craft distinct pet icons each time using OpenAI's DALL-E2 technology, ensuring unparalleled designs for your endeavors.</p>
+                        <GoGear className='text-4xl dark:text-white font-bold' />
+                        <h1 className='text-3xl font-bold dark:text-white mb-5'>High-Quality Output</h1>
+                        <p className='text-lg dark:text-white'>Grab your pet icons in crisp high-resolution PNG format for web use or share direct links with your friends.</p>
                     </div>
                 </div>
             </div>
