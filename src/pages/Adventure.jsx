a36cefe921ccc4081360da0d5a9f3be2b92acc35
Dagim Asnake <dagimasnake1@gmail.com>
2023-11-27 07:34:55 -1200

    added adventure

---
+++ src/pages/Adventure.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import shot from "../assets/Shot.png"
+import { RiRobot2Line } from "react-icons/ri";
+
+const Adventure = () => {
+  return (
+    <>
+        <div className='bg-gray-100 dark:bg-gray-900 mt-10 px-4 pb-16 md:px-10 lg:px-56'>
+            <h1 className='text-4xl md:text-5xl pt-16 pb-12 md:pb-24 font-semibold dark:text-white text-center'>Em-bark on Icon Adventures with PetGen!</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-14' > 
+                <div>
+                    <RiRobot2Line className='text-4xl dark:text-white font-bold' />
+                    <h1 className='text-2xl font-bold dark:text-white mb-5'>Create your own pets!</h1>
+                    <p className='text-base md:text-2xl dark:text-white'>Unleash your creativity with our AI pet icon generator! Choose your beloved pet, breed, style, colour, and pose to craft the perfect, unique pet icon. And the best part? You can generate a whole menagerie of these charming icons effortlessly, infusing endless animal magic into your projects!</p>
+                </div>
+
+                <div className='col-span-2'>
+                    <img src={shot} alt="" className='h-full w-full rounded-3xl' />
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Adventure
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import Image from './Image'
 import Features from './Features'
+import Adventure from './Adventure'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Hero />
     <Image />
     <Features />
+    <Adventure />
     </>
   )
 }
